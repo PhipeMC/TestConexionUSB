2771312f03bbc5f00647206c6566ca3becb712b2
PhipeMC <phipe.mcbukkit@gmail.com>
2022-03-24 00:01:58 -0600

    Lista de dispositivos USB con sus datos

--- app/src/main/java/com/example/testconexionusb/MainActivity.java
+++ app/src/main/java/com/example/testconexionusb/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.testconexionusb;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
+import android.net.ParseException;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -18,59 +25,148 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String ACTION_USB_PERMISSION = "com.example.testconexionusb.USB_PERMISSION";
+    private UsbDevice usbD;
     UsbManager usbManager;
-    private static final String ACTION_USB_PERMISSION =
-            "com.android.example.USB_PERMISSION";
-    //UsbDevice device;
+    private TextView mStatusView, mResultView;
+    private static String TAG = "USB-Devices";
 
-    /*private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (ACTION_USB_PERMISSION.equals(action)) {
-                synchronized (this) {
-                    device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-
-                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-                        if(device != null){
-                            Toast.makeText(context, "hay un USB conectado", Toast.LENGTH_SHORT).show();
-                            Toast.makeText(context, device.getDeviceName(), Toast.LENGTH_SHORT).show();
-                            //call method to set up device communication
-                        }
-                    }
-                    else {
-                        Log.d("permiso", "permission denied for device " + device);
-                    }
-                }
-            }
-        }
-    };*/
-
+    @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mStatusView = (TextView) findViewById(R.id.text_status);
+        mResultView = (TextView) findViewById(R.id.text_result);
+
         usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
 
-        HashMap<String, UsbDevice> deviceList = usbManager.getDeviceList();
+        /*HashMap<String, UsbDevice> deviceList = usbManager.getDeviceList();
         Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
-        Log.d("usb", deviceList.size()+"");
+        Log.d("USB-Log", "Lista" + Integer.toString(deviceList.size()));
         System.out.println(deviceList.size());
         while(deviceIterator.hasNext()){
             UsbDevice device = deviceIterator.next();
-            Log.d("usb", device.getDeviceName());
+            usbD = device;
+            Log.d("USB-Log", device.getDeviceName());
         }
 
 
+        PendingIntent permissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        registerReceiver(usbReceiver, filter);
+        usbManager.requestPermission(usbD, permissionIntent);*/
+        IntentFilter filter = new IntentFilter(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(usbReceiver, filter);
 
-//        PendingIntent permissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
-//        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
-//        registerReceiver(usbReceiver, filter);
-        //usbManager.requestPermission(device, permissionIntent);
+        handleIntent(getIntent());
+    }
 
+    private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
 
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if(device != null){
 
+                        }
+                    }
+                    else {
+                        Log.d("USB-Broad", "permission denied for device " + device);
+                    }
+                }
+            }
+        }
+    };
 
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private void handleIntent(Intent intent) {
+        UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+        if (device != null) {
+            printStatus(getString(R.string.status_added));
+            printDeviceDetails(device);
+        } else {
+            // List all devices connected to USB host on startup
+            printStatus(getString(R.string.status_list));
+            printDeviceList();
+        }
+    }
+
+    private void printDeviceList() {
+        HashMap<String, UsbDevice> connectedDevices = usbManager.getDeviceList();
+
+        if (connectedDevices.isEmpty()) {
+            printResult("No Devices Currently Connected");
+        } else {
+            StringBuilder builder = new StringBuilder();
+            builder.append("Connected Device Count: ");
+            builder.append(connectedDevices.size());
+            builder.append("\n\n");
+            for (UsbDevice device : connectedDevices.values()) {
+                //Use the last device detected (if multiple) to open
+                builder.append(UsbHelper.readDevice(device));
+                builder.append("\n\n");
+            }
+            printResult(builder.toString());
+        }
+    }
+
+    /**
+     * Print a basic description about a specific USB device.
+     * @param device USB device to query.
+     */
+    private void printDeviceDescription(UsbDevice device) {
+        String result = UsbHelper.readDevice(device) + "\n\n";
+        printResult(result);
+    }
+
+    /**
+     * Initiate a control transfer to request the device information
+     * from its descriptors.
+     *
+     * @param device USB device to query.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private void printDeviceDetails(UsbDevice device) {
+        UsbDeviceConnection connection = usbManager.openDevice(device);
+
+        String deviceString = "";
+        try {
+            //Parse the raw device descriptor
+            deviceString = DeviceDescriptor.fromDeviceConnection(connection)
+                    .toString();
+        } catch (IllegalArgumentException e) {
+            Log.w(TAG, "Invalid device descriptor", e);
+        }
+
+        String configString = "";
+        try {
+            //Parse the raw configuration descriptor
+            configString = ConfigurationDescriptor.fromDeviceConnection(connection)
+                    .toString();
+        } catch (IllegalArgumentException e) {
+            Log.w(TAG, "Invalid config descriptor", e);
+        } catch (ParseException e) {
+            Log.w(TAG, "Unable to parse config descriptor", e);
+        }
+
+        printResult(deviceString + "\n\n" + configString);
+        connection.close();
+    }
+
+    /* Helpers to display user content */
+
+    private void printStatus(String status) {
+        mStatusView.setText(status);
+        Log.i(TAG, status);
+    }
+
+    private void printResult(String result) {
+        mResultView.setText(result);
+        Log.i(TAG, result);
     }
 }
